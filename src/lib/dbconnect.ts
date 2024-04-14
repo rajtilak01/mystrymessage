ecd79b6fb029526204fe8d64bb82f5ef99823f9d
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2024-04-14 16:10:49 +0530

    database connection

---
+++ src/lib/dbconnect.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number;
+}
+
+
+const connection : ConnectionObject = {
+
+}
+
+async function dbConnect():Promise<void> {
+    if(connection.isConnected) {
+        console.log("db connection already done");
+        return;
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URL || "");
+
+        connection.isConnected = db.connections[0].readyState;
+        console.log("db connection done", db);
+
+
+    } catch (error:any){
+        console.log("Database connection failed", error);
+        process.exit(1)
+        
+    }
+
+}
+
+export default dbConnect;
