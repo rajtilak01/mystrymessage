bd254b07b6b0709b3b8e15c0a862c6c72ff7544a
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2024-05-22 23:51:16 +0530

    shadcn form frontend

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,42 @@
 'use client'
-import { useSession, signIn, signOut } from "next-auth/react"
+import React, { useState } from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import Link from 'next/link'
+import { useDebounceValue } from "usehooks-ts"
+import { useToast } from "@/components/ui/use-toast"
+import { useRouter } from 'next/navigation'
+import { signInSchema } from '@/schemas/signInSchema'
+import { signUpSchema } from '@/schemas/signUpSchemas'
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+
+function page() {
+  const [username, setUsername] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [ischeckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, useIsSubmitting] = useState(false);
+
+  const debouncedUsername = useDebounceValue(username, 300);
+
+  const { toast } = useToast();
+  const router = useRouter();
+
+  //zod implementation
+
+  const form = useForm({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: '',
+      email: '',
+      password: '',
+    }
+  });
+
+
   return (
-    <>
-      Not signed in <br />
-      <button className="bg-orange-500" onClick={() => signIn()}>Sign in</button>
-    </>
+    <div>page</div>
   )
 }
+
+export default page
