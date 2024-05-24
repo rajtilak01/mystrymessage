c6b6dad107f98bc3432fb2912f5a5ebc1dcdfe16
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2024-05-24 23:54:22 +0530

    sign in frontend done

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -17,7 +17,7 @@ function page() {
   const [username, setUsername] = useState("");
   const [usernameMessage, setUsernameMessage] = useState("");
   const [ischeckingUsername, setIsCheckingUsername] = useState(false);
-  const [isSubmitting, useIsSubmitting] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const debouncedUsername = useDebounceValue(username, 300);
 
@@ -52,11 +52,81 @@ function page() {
         }
       }
     }
-  }, [debouncedUsername])
+    checkUsernameUnique();
+  }, [debouncedUsername]);
+
+  const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+    setIsSubmitting(true);
+    try {
+      const response = await axios.post<ApiResponse>('/api/sign-up', data);
+      toast({
+        title: "Success",
+        description: response.data.message
+      })
+      router.replace(`/verify/${username}`);
+      setIsSubmitting(false);
+    } catch (error) {
+      console.error("error in signup of user", error);
+      const axiosError = error as AxiosError<ApiResponse>;
+      let errorMessage = axiosError.response?.data.message 
+      toast({
+        title: "Signup failed",
+        description: errorMessage,
+        variant: "destructive",
+      })
+      setIsSubmitting(false);
+    }
+  
+  }
 
   return (
-    <div>page</div>
-  )
+    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Welcome Back to True Feedback
+          </h1>
+          <p className="mb-4">Sign in to continue your secret conversations</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className='w-full' type="submit">Sign In</Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Not a member yet?{' '}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+
 }
 
 export default page
